609d0743fe064eca075ddd2ef446bcec192c6100
PC <https://github.com/duyquan27/Smart_Home_Mobile>
2023-10-09 11:59:10 +0700

    Hiep

---
+++ app/src/main/java/com/example/afinal/fragment_home/fragmentHistory.java
@@ -0,0 +1,22 @@
+package com.example.afinal.fragment_home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.afinal.R;
+
+public class fragmentHistory extends Fragment {
+
+    private View mView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.fragment_lich_su, container, false);
+        return mView;
+    }
+}
