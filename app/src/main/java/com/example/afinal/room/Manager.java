f2e3af9c50be61e813fddff6365f4e97a4aa2562
PC <https://github.com/duyquan27/Smart_Home_Mobile>
2023-10-18 00:41:54 +0700

    Hiep update addmember

--- app/src/main/java/com/example/afinal/room/Manager.java
+++ app/src/main/java/com/example/afinal/room/Manager.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
 import com.example.afinal.R;
 import com.example.afinal.room.member.memberFragment;
 import com.google.android.material.tabs.TabLayout;
@@ -29,5 +30,6 @@ public class Manager extends AppCompatActivity {
 
         mViewPager.setAdapter(viewPagerManager);
         mTableLayout.setupWithViewPager(mViewPager);
+
     }
 }
--- app/src/main/java/com/example/afinal/room/member/add_member.java
+++ app/src/main/java/com/example/afinal/room/member/add_member.java
@@ -3,45 +3,61 @@ package com.example.afinal.room.member;
 import android.widget.ImageView;
 
 public class add_member {
-    private int imageView_sex;
-    private String name_member, starttime, textphone;
+    private String name_member, gender_member, date_Member, address_Member, starttime_member, phone_member;
 
-    public String getName_member() {
-        return name_member;
+    public add_member() {
+
+    }
+    public add_member(String name_member, String gender_member, String date_Member, String address_Member, String starttime_member, String phone_member) {
+        this.name_member = name_member;
+        this.gender_member = gender_member;
+        this.date_Member = date_Member;
+        this.address_Member = address_Member;
+        this.starttime_member = starttime_member;
+        this.phone_member = phone_member;
     }
 
     public void setName_member(String name_member) {
         this.name_member = name_member;
     }
 
-    public int getImageView() {
-        return imageView_sex;
+    public void setGender_member(String gender_member) {
+        this.gender_member = gender_member;
     }
 
-    public void setImageView(int imageView) {
-        imageView_sex = imageView;
+    public void setDate_Member(String date_Member) {
+        this.date_Member = date_Member;
     }
 
-    public String getStarttime() {
-        return starttime;
+    public void setAddress_Member(String address_Member) {
+        this.address_Member = address_Member;
     }
 
-    public void setStarttime(String starttime) {
-        this.starttime = starttime;
+    public void setStarttime_member(String starttime_member) {
+        this.starttime_member = starttime_member;
     }
 
-    public String getTextphone() {
-        return textphone;
+    public void setPhone_member(String phone_member) {
+        this.phone_member = phone_member;
     }
 
-    public void setTextphone(String textphone) {
-        this.textphone = textphone;
+    public String getName_member() {
+        return name_member;
+    }
+    public String getGender_member() {
+        return gender_member;
+    }
+    public String getDate_Member() {
+        return date_Member;
+    }
+    public String getAddress_Member() {
+        return address_Member;
+    }
+    public String getStarttime_member() {
+        return starttime_member;
+    }
+    public String getPhone_member() {
+        return phone_member;
     }
 
-    public add_member(int imageView_sex, String name_member, String starttime, String textphone) {
-        this.imageView_sex = imageView_sex;
-        this.name_member = name_member;
-        this.starttime = starttime;
-        this.textphone = textphone;
-    }
 }
